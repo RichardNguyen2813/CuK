43c6851ce68b2de9c37933d9b8a246b8c20c9c54
RichardNguyen2813 <ndkhoi21@clc.fitus.edu.vn>
2023-06-23 12:15:17 +0700

    ud

--- pages/index.js
+++ pages/index.js
@@ -6,21 +6,29 @@ export default function Home() {
 
   if (!session) {
     return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 px-4 rounded-lg"
-          >
-            Login with Google
-          </button>
-        </div>
+      <div>
+        <head>
+          <title> ST You Need</title>
+        </head>
+        <main className="bg-gray-200 rounded-xl p-8 dark:bg-slate-800 h-screen w-screen grid justify-items-center items-center  ">
+          
+          <div className="text-center p-6 px-4 rounded-lg bg-white ">
+            <p className="p-2">Hi, we are trying to build a web project</p>
+            <button
+              onClick={() => signIn("google")}
+              className="bg-gray-300 p-2 px-4 rounded-lg"
+            >
+              Login with Google
+            </button>
+          </div>
+          
+        </main>
       </div>
     );
   }
 
   return (
-    <div className="bg-blue-900 min-h-screen flex">
+    <div className="bg-gray-200 min-h-screen flex">
       <Nav />
       <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
         logged in {session.user.email}
